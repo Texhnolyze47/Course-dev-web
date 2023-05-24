e68f62a0fecd7935bfb02d731a7ada50a62cc339
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-24 16:49:35 -0600

    add: navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+function navbar() {
+    return (
+        <nav className="bg-gray-100 flex items-center ">
+            <img className="block h-12 w-12" src="src\assets\logo\logo.jpeg" alt="Logo de la empresa" />
+            <ul className="flex-auto pl-4">Course-dev</ul>
+            <ul className="flex-auto">Plan de Estudio</ul>
+            <ul className="flex-auto">Categorias</ul>
+            <ul className="flex-auto">Blog</ul>
+            <ul className="flex-auto">Planes</ul>
+            <ul className="flex justify-end pr-4">Iniciar Sesion</ul>
+            <ul className="flex justify-end pr-4">Registrarse</ul>
+        </nav>
+    )
+}
+
+export default navbar;
