60e96c5bf88effda1278240d483e9e0e5ae0e0a9
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-25 13:20:12 -0600

    add: creando seccion seccion de partners

---
+++ src/components/Partners.jsx
@@ -0,0 +1,13 @@
+function parthers(){
+    return(
+        <div className="flex items-center">
+            <h1 className="flex-auto pl-6 w-20 text-2xl	">Nuestros partners e inversores de confianza son un pilar fundamental en nuestro éxito. </h1>
+
+            <img src="src/assets/images/macquarie_university.png" className="w-60 grayscale" alt="" />
+            <img src="src/assets/images/Emory-University-Logo.png" className="w-60 grayscale" alt=""  />
+            <img src="src/assets/images/the-university-of-sydney-3-logo.png" className="w-60 grayscale" alt=""  />
+        </div>
+    )
+}
+
+export default parthers;
