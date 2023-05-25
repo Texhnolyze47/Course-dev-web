be8c48ba60c6760181cb75196250d5096f0ee0dd
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-25 15:39:36 -0600

    add: deno dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: deno()
 });
