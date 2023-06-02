4697f7174449e4db236e49b0b07745ce80ebd3ac
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-02 10:20:55 -0600

    nodejs imports faltantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
-import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
-  output: "server",
-  adapter: deno()
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
