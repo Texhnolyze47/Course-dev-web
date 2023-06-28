808ef7cb3c0dcb708b227fd78d87d460e61a7fcb
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-27 23:33:09 -0600

    Se termino cardlist de cursos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,21 @@
 
-
-export default function Card({ title, description, image, category }) {
-
+export default function Card({ title, price, image, category }) {
 
   return (
-    <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden">
-      <img className="h-48 object-cover" src={image} alt={title} />
-      <div className="p-4">
-        <div className="flex flex-col">
-          <p className="mt-2 text-gray-600">{category}</p>
-          <h2 className="text-lg font-medium text-gray-900">{title}</h2>
+    <div class="flex flex-col bg-white rounded-lg shadow-md px-4">
+      <img class="h-48 object-cover" src={image} alt={title} />
+      <div class="p-4 ">
+        <div class="flex flex-col">
+          <p class="mt-2 text-gray-600">{category}</p>
+          <h2 class="text-lg font-medium text-gray-900">{title}</h2>
         </div>
-        <p className="mt-2 text-gray-600">{description}</p>
-        <div className="flex">
-          <p className="self-center float-left">$200.0</p>
-          <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 float-right rounded">
-            Click me
+        <p class="mt-2 text-gray-600">{price}</p>
+        <div class="flex justify-between items-center mt-0">
+          <p class="">$200.0</p>
+          <button class="bg-stone-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Comienza
           </button>
         </div>
-
       </div>
     </div>
   );
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,66 +1,102 @@
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect } from "solid-js";
+import { FaSolidArrowLeft, FaSolidArrowRight } from 'solid-icons/fa';
+import Card  from "./Card";
+
+
+const categories = [
+  { name: "All", filter: () => true },
+  { name: "Web Design", filter: (item) => item.category === "Web Design" },
+  { name: "UI/UX", filter: (item) => item.category === "UI/UX" },
+  { name: "Coding", filter: (item) => item.category === "Coding" }
+];
 
 function CardList() {
-  const [selectedCategory, setSelectedCategory] = createSignal('all');
+  const [selectedCategory, setSelectedCategory] = createSignal(categories[0]);
+  const [items, setItems] = createSignal([
+    { name: "Item 1", category: "Web Design", price: 200, image: "https://picsum.photos/200" },
+    { name: "Item 2", category: "UI/UX", price: 300, image: "https://picsum.photos/200" },
+    { name: "Item 3", category: "Web Design", price: 400, image: "https://picsum.photos/200" },
+    { name: "Item 4", category: "Coding", price: 500, image: "https://picsum.photos/200" },
+    { name: "Item 5", category: "UI/UX", price: 600, image: "https://picsum.photos/200" },
+    { name: "Item 6", category: "Coding", price: 700, image: "https://picsum.photos/200" },
+    { name: "Item 7", category: "Web Design", price: 800, image: "https://picsum.photos/200" },
+    { name: "Item 8", category: "UI/UX", price: 900, image: "https://picsum.photos/200" },
+    { name: "Item 9", category: "Coding", price: 1000, image: "https://picsum.photos/200" },
+    { name: "Item 10", category: "Web Design", price: 1100, image: "https://picsum.photos/200" },
+  ]);
 
-  // Sample data
-  const items = [
-    { id: 1, name: 'Item 1', category: 'cat1' },
-    { id: 2, name: 'Item 2', category: 'cat2' },
-    { id: 3, name: 'Item 3', category: 'cat1' },
-    { id: 4, name: 'Item 4', category: 'cat3' },
-    { id: 5, name: 'Item 5', category: 'cat2' },
-  ];
+  const [currentPage, setCurrentPage] = createSignal(1);
+  const itemsPerPage = 3;
 
-  const filteredItems = selectedCategory() === 'all'
-    ? items
-    : items.filter(item => item.category === selectedCategory());
+  createEffect(() => {
+    setCurrentPage(1);
+  });
+
+  function handleClick(category) {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+  }
+
+  function filteredItems(items) {
+    const startIndex = (currentPage() - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return items.filter(selectedCategory().filter).slice(startIndex, endIndex);
+  }
 
   return (
     <div>
       <div class="space-x-2">
-        <button
-          class="px-2 py-1 rounded bg-blue-500 text-white"
-          onClick={() => setSelectedCategory('all')}
-          classList={{ 'bg-blue-700': selectedCategory() === 'all' }}
-        >
-          All
-        </button>
-        <button
-          class="px-2 py-1 rounded bg-blue-500 text-white"
-          onClick={() => setSelectedCategory('cat1')}
-          classList={{ 'bg-blue-700': selectedCategory() === 'cat1' }}
-        >
-          Category 1
-        </button>
-        <button
-          class="px-2 py-1 rounded bg-blue-500 text-white"
-          onClick={() => setSelectedCategory('cat2')}
-          classList={{ 'bg-blue-700': selectedCategory() === 'cat2' }}
-        >
-          Category 2
-        </button>
-        <button
-          class="px-2 py-1 rounded bg-blue-500 text-white"
-          onClick={() => setSelectedCategory('cat3')}
-          classList={{ 'bg-blue-700': selectedCategory() === 'cat3' }}
-        >
-          Category 3
-        </button>
+        {categories.map((category) => (
+          <button
+            classList={{
+             "px-2 py-1 rounded text-black	": true,
+            "opacity-50 font-extrabold": category === selectedCategory()
+            }}
+            onclick={() => handleClick(category)}
+          >
+            {category.name}
+          </button>
+        ))}
       </div>
 
-      <ul class="mt-4">
-        {filteredItems.map(item => (
-          <li key={item.id} class="py-2">
-            {item.name}
-            {console.log(item.name)}
-
-          </li>
+      <div class="grid grid-cols-3 mt-4">
+        {filteredItems(items()).map((item) => (
+          <Card
+            title={item.name}
+            description={`$${item.price}`}
+            image={item.image}
+            category={item.category}
+          />
         ))}
-        {console.log(filteredItems)}
-      </ul>
+      </div>
+
+      <div class="flex flex-row items-center justify-center py-2 ">
+        <FaSolidArrowLeft class="mr-2"
+          disabled={currentPage() === 1}
+          onclick={() => setCurrentPage(currentPage() - 1)}
+        >
+        </FaSolidArrowLeft>
+        <span>
+          Page {currentPage()} of{" "}
+          {Math.ceil(
+            items().filter(selectedCategory().filter).length / itemsPerPage
+          )}
+        </span>
+        <FaSolidArrowRight class="ml-2"
+          disabled={
+            currentPage() ===
+            Math.ceil(
+              items().filter(selectedCategory().filter).length / itemsPerPage
+            )
+          }
+          onclick={() => setCurrentPage(currentPage() + 1)}
+        >
+          Next Page
+        </FaSolidArrowRight>
+      </div>
     </div>
   );
 }
+
 
 export default CardList;
