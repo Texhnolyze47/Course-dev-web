745bd2ea5193936935ad63ad4a4e5b4cb6e419bb
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-28 10:57:06 -0600

    add: preparando para el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import node from '@astrojs/node';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind(), mdx()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify()
 });
