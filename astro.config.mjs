1420e13631bc24975dcb9858f1798d70c6f737d3
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-29 22:29:27 -0600

    Se quito dependecias inncessario y actualizo solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
 import mdx from "@astrojs/mdx";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [solidJs(), tailwind(), mdx()],
-  output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });
